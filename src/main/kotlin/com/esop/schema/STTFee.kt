cf74f3260edb56f780dab47da78701f6c56f0260
3Anushka <anushkaj@sahaj.ai>
2023-02-16 11:43:02 +0530

    Add class for stt fee

---
+++ src/main/kotlin/com/esop/schema/STTFee.kt
@@ -0,0 +1,25 @@
+package com.esop.schema
+
+import com.esop.STTFeeLessThanZeroException
+import java.math.BigInteger
+
+class STTFee {
+
+        companion object {
+
+            var totalSTTFee: BigInteger = BigInteger("0")
+
+            fun addSTTFee(fee: Long) {
+                if (fee < 0)
+                    throw STTFeeLessThanZeroException()
+                totalSTTFee += fee.toBigInteger()
+            }
+
+            fun getSTTFee(): BigInteger {
+                return totalSTTFee
+            }
+
+        }
+
+
+    }
