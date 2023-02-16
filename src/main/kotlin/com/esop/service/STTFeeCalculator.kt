a2b681deebf43a0906f177bdbd40e9ec381ead62
3Anushka <anushkaj@sahaj.ai>
2023-02-16 16:43:55 +0530

    Change of constructor parameter to sellAmount in STTFeeCalculator
    -taken order transaction amount instead of order price

--- src/main/kotlin/com/esop/service/STTFeeCalculator.kt
+++ src/main/kotlin/com/esop/service/STTFeeCalculator.kt
@@ -7,22 +7,21 @@ import kotlin.math.round
 class STTFeeCalculator {
 
 
-    fun calculate(price: Long, esopType: String, quantity: Long): Long {
-        val sellAmount = price * quantity
+    fun calculate(sellAmount: Long, esopType: String, sellQuantity: Long): Long {
         var securityTransactionFee = 0L
         if (esopType == "NON_PERFORMANCE") {
-            securityTransactionFee = if (quantity <= MAX_ESPO_QUANTITY_SLAB_ONE) {
+            securityTransactionFee = if (sellQuantity <= MAX_ESPO_QUANTITY_SLAB_ONE) {
                 min(round(sellAmount * NON_PERFORMANCE_SLAB_1_PERCENTAGE).toLong(), NON_PERFORMANCE_CAP_AMOUNT)
-            } else if (quantity < MAX_ESPO_QUANTITY_SLAB_TWO) {
+            } else if (sellQuantity < MAX_ESPO_QUANTITY_SLAB_TWO) {
                 min(round(sellAmount * NON_PERFORMANCE_SLAB_2_PERCENTAGE).toLong(), NON_PERFORMANCE_CAP_AMOUNT)
             } else {
                 round(sellAmount * NON_PERFORMANCE_SLAB_3_PERCENTAGE).toLong()
             }
         }
         if (esopType == "PERFORMANCE") {
-            securityTransactionFee = if (quantity <= MAX_ESPO_QUANTITY_SLAB_ONE) {
+            securityTransactionFee = if (sellQuantity <= MAX_ESPO_QUANTITY_SLAB_ONE) {
                 min(round(sellAmount * PERFORMANCE_SLAB_1_PERCENTAGE).toLong(), PERFORMANCE_CAP_AMOUNT)
-            } else if (quantity < MAX_ESPO_QUANTITY_SLAB_TWO) {
+            } else if (sellQuantity < MAX_ESPO_QUANTITY_SLAB_TWO) {
                 min(round(sellAmount * PERFORMANCE_SLAB_2_PERCENTAGE).toLong(), PERFORMANCE_CAP_AMOUNT)
             } else {
                 round(sellAmount * PERFORMANCE_SLAB_3_PERCENTAGE).toLong()
--- src/test/kotlin/com/esop/service/STTFeeCalculatorTest.kt
+++ src/test/kotlin/com/esop/service/STTFeeCalculatorTest.kt
@@ -7,7 +7,7 @@ class STTFeeCalculatorTest {
     @Test
     fun `it should calculate STTFee for NON_PERFORMANCE Esop with trade quantity less than 100 with cap of RS 20`() {
 
-        val securityTransactionTax = STTFeeCalculator().calculate(100, "NON_PERFORMANCE", 100)
+        val securityTransactionTax = STTFeeCalculator().calculate(10000, "NON_PERFORMANCE", 100)
 
         assertEquals(20, securityTransactionTax)
     }
@@ -22,7 +22,7 @@ class STTFeeCalculatorTest {
     @Test
     fun `it should calculate STTFee for NON_PERFORMANCE Esop with trade quantity between 100 and 50K`() {
 
-        val securityTransactionTax = STTFeeCalculator().calculate(1, "NON_PERFORMANCE", 300)
+        val securityTransactionTax = STTFeeCalculator().calculate(300, "NON_PERFORMANCE", 300)
 
         assertEquals(4, securityTransactionTax)
 
@@ -31,14 +31,14 @@ class STTFeeCalculatorTest {
     @Test
     fun `it should calculate STTFee for NON_PERFORMANCE Esop with trade quantity between 100 and 50K with cap of RS 20`() {
 
-        val securityTransactionTax = STTFeeCalculator().calculate(100, "NON_PERFORMANCE", 300)
+        val securityTransactionTax = STTFeeCalculator().calculate(30000, "NON_PERFORMANCE", 300)
 
         assertEquals(20, securityTransactionTax)
     }
 
     @Test
     fun `it should calculate STTFee for NON_PERFORMANCE Esop with trade quantity greater than 50K`() {
-        val securityTransactionTax = STTFeeCalculator().calculate(100, "NON_PERFORMANCE", 60000)
+        val securityTransactionTax = STTFeeCalculator().calculate(6000000, "NON_PERFORMANCE", 60000)
 
         assertEquals(90000, securityTransactionTax)
 
@@ -47,7 +47,7 @@ class STTFeeCalculatorTest {
 
     @Test
     fun `it should calculate STTFee for PERFORMANCE Esop with trade quantity less than 100 with cap of RS 50`() {
-        val securityTransactionTax = STTFeeCalculator().calculate(100, "PERFORMANCE", 100)
+        val securityTransactionTax = STTFeeCalculator().calculate(10000, "PERFORMANCE", 100)
 
         assertEquals(50, securityTransactionTax)
 
@@ -55,28 +55,28 @@ class STTFeeCalculatorTest {
 
     @Test
     fun `it should calculate STTFee for PERFORMANCE Esop with trade quantity less than 100 `() {
-        val securityTransactionTax = STTFeeCalculator().calculate(1, "PERFORMANCE", 100)
+        val securityTransactionTax = STTFeeCalculator().calculate(100, "PERFORMANCE", 100)
 
         assertEquals(2, securityTransactionTax)
     }
 
     @Test
     fun `it should calculate STTFee for PERFORMANCE Esop with trade quantity between 100 and 50K`() {
-        val securityTransactionTax = STTFeeCalculator().calculate(1, "PERFORMANCE", 150)
+        val securityTransactionTax = STTFeeCalculator().calculate(150, "PERFORMANCE", 150)
 
         assertEquals(3, securityTransactionTax)
     }
 
     @Test
     fun `it should calculate STTFee for PERFORMANCE Esop with trade quantity between 100 and 50K with cap of RS 50`() {
-        val securityTransactionTax = STTFeeCalculator().calculate(100, "PERFORMANCE", 350)
+        val securityTransactionTax = STTFeeCalculator().calculate(35000, "PERFORMANCE", 350)
 
         assertEquals(50, securityTransactionTax)
     }
 
     @Test
     fun `it should calculate STTFee for PERFORMANCE Esop with trade quantity greater than 50K`() {
-        val securityTransactionTax = STTFeeCalculator().calculate(1, "PERFORMANCE", 60000)
+        val securityTransactionTax = STTFeeCalculator().calculate(60000, "PERFORMANCE", 60000)
 
         assertEquals(1500, securityTransactionTax)
     }
