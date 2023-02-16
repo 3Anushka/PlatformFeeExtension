d05ec66263c95b292eb643ee0cb14c5df4279cfd
3Anushka <anushkaj@sahaj.ai>
2023-02-16 11:45:36 +0530

    Remove warning

--- src/main/kotlin/com/esop/schema/STTFee.kt
+++ src/main/kotlin/com/esop/schema/STTFee.kt
@@ -5,21 +5,21 @@ import java.math.BigInteger
 
 class STTFee {
 
-        companion object {
+    companion object {
 
-            var totalSTTFee: BigInteger = BigInteger("0")
+        private var totalSTTFee: BigInteger = BigInteger("0")
 
-            fun addSTTFee(fee: Long) {
-                if (fee < 0)
-                    throw STTFeeLessThanZeroException()
-                totalSTTFee += fee.toBigInteger()
-            }
-
-            fun getSTTFee(): BigInteger {
-                return totalSTTFee
-            }
-
+        fun addSTTFee(fee: Long) {
+            if (fee < 0)
+                throw STTFeeLessThanZeroException()
+            totalSTTFee += fee.toBigInteger()
         }
 
+        fun getSTTFee(): BigInteger {
+            return totalSTTFee
+        }
 
     }
+
+
+}
